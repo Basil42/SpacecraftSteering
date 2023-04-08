9cbebdc0d77bfd84909b22e3dcf664d46bc75a57
Basil42 <32388487+Basil42@users.noreply.github.com>
2023-04-08 16:15:58 +0200

    heading control

--- Assets/Steering/AttitudeController.cs
+++ Assets/Steering/AttitudeController.cs
@@ -1,12 +1,9 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 //TODO: take a target attitude using quaternion.lookatRotation, extract an up and forward vector from it and run a third PID controller for forward attitude, they could balance out correctly
 [RequireComponent(typeof(Rigidbody))]
-public class AttitudeController : MonoBehaviour, INeutralRollController,IRollController,IHeadingController
+public class AttitudeController : MonoBehaviour, INeutralRollController,IHeadingController
 {
     [Header("Ship properties")]//probably store these in a SO instead of serializing here
     [SerializeField]private float attitudeAuthority = 150.0f;
@@ -19,6 +16,7 @@ public class AttitudeController : MonoBehaviour, INeutralRollController,IRollCon
     [SerializeField] private float targetAngularVelocityTolerance = 0.5f;//This could be replaced by a dynamic value equal to the maximum delta of angular velocity the object can produce in a physics tick
     private Transform _transform;
     private Rigidbody _rb;
+
     private void Awake()
     {
         _transform = transform;
@@ -52,16 +50,23 @@ public class AttitudeController : MonoBehaviour, INeutralRollController,IRollCon
         RollToDownDirection(-up);
     }
 
+    private Coroutine _rollRoutine;
     public void RollToDownDirection(Vector3 down)
     {
         StopAllCoroutines();
-        StartCoroutine(RollToDownDirectionRoutine(down));
+        angularSpeedDampingController.ResetIntegral();//TODO move in OnEnable
+        _rollRoutine = StartCoroutine(RollToDownDirectionRoutine(down));
+    }
+
+    public void StopRoll()
+    {
+        //temporary implementation, mostly for debug purposes
+        _rb.angularVelocity = Vector3.zero;
     }
 
     private IEnumerator RollToDownDirectionRoutine(Vector3 down)
     {
         rollController.ResetIntegral();
-        angularSpeedDampingController.ResetIntegral();
         Vector3 rollPidValue;
         Vector3 angularSpeedDampeningPidValue;
         Vector3 rollError;
@@ -79,13 +84,14 @@ public class AttitudeController : MonoBehaviour, INeutralRollController,IRollCon
             }
 
             errorAngle = Vector3.Angle(currentDownDirection, down);
-            errorFactor = -(Mathf.Cos((errorAngle/180f)*Mathf.PI)-1f);
+            errorFactor = errorAngle/180f;
             rollError = rollError.normalized * errorFactor;
                         //we use cross product to get the torque axis, but we remap it's magnitude to be 2 at 180 rather than 0
             rollPidValue = rollController.Tick(rollError, Time.fixedDeltaTime);
             
             angularSpeedError = -_rb.angularVelocity;
             angularSpeedDampeningPidValue = angularSpeedDampingController.Tick(angularSpeedError, Time.fixedDeltaTime);
+            var totalPidValue = rollPidValue + angularSpeedDampeningPidValue;
             var torqueValue = Vector3.ClampMagnitude(rollPidValue + angularSpeedDampeningPidValue, attitudeAuthority);
             torqueValue = _transform.InverseTransformDirection(torqueValue);
             _rb.AddRelativeTorque(torqueValue);
@@ -94,7 +100,7 @@ public class AttitudeController : MonoBehaviour, INeutralRollController,IRollCon
         } while (rollError.magnitude > errorTolerance ||
                  _rb.angularVelocity.magnitude > targetAngularVelocityTolerance);
         Debug.Log("Maneuver complete");
-        _rb.angularVelocity = Vector3.zero;
+        _rb.angularVelocity = Vector3.zero;//TODO: move in Ondisable
     }
 
     public void TurnToward(Vector3 worldPosition)
@@ -102,21 +108,65 @@ public class AttitudeController : MonoBehaviour, INeutralRollController,IRollCon
         ChangeHeadingTo((worldPosition - _transform.position).normalized);
     }
 
-    
+    private Coroutine _headingCoroutine;
     public void ChangeHeadingTo(Vector3 direction)
     {
-        
+        if (_headingCoroutine != null)
+        {
+            StopCoroutine(_headingCoroutine);
+            _headingCoroutine = null;
+        }
+        angularSpeedDampingController.ResetIntegral();//not super happy about that
+
+        StartCoroutine(ChangeHeadingRoutine(direction));
     }
+
+    private IEnumerator ChangeHeadingRoutine(Vector3 direction)
+    {
+        headingController.ResetIntegral();
+        Vector3 headingPidValue;
+        //not doing the speed dampening here, I'll move it to update
+        Vector3 headingError;
+        float errorFactor;
+        float errorAngle;
+        WaitForFixedUpdate waiter = new WaitForFixedUpdate();
+        do
+        {
+            var currentHeading = _transform.forward;
+            headingError = Vector3.Cross(currentHeading, direction);
+            if (headingError.magnitude < errorTolerance &&
+                Mathf.Abs(Vector3.Angle(currentHeading, direction) - 180.0f) < errorTolerance)
+            {
+                headingError = transform.up;
+            }
+
+            errorAngle = Vector3.Angle(currentHeading, direction);
+            errorFactor = errorAngle / 180.0f;
+            headingError = headingError.normalized * errorFactor;
+            headingPidValue = headingController.Tick(headingError, Time.fixedDeltaTime);
+            var torqueValue = Vector3.ClampMagnitude(headingPidValue, attitudeAuthority);//TODO: check both x and y axis separately
+            torqueValue = _transform.InverseTransformDirection(torqueValue);
+            _rb.AddRelativeTorque(torqueValue);
+            yield return waiter;
+        } while (headingError.magnitude > errorTolerance ||
+                 _rb.angularVelocity.magnitude > targetAngularVelocityTolerance);
+
+        _headingCoroutine = null;
+        Debug.Log("heading maneuver completed");
+    }
+    
 }
 
 public interface IRollController
 {
     public void RollToUpDirection(Vector3 up);
     public void RollToDownDirection(Vector3 down);
-    
+
+    public void StopRoll();
+
 }
 
-public interface INeutralRollController
+public interface INeutralRollController : IRollController
 {
     public void RollToNeutral();
 }
--- Assets/Steering/SteeringInputMockup.cs
+++ Assets/Steering/SteeringInputMockup.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Serialization;
@@ -8,23 +9,37 @@ using Random = UnityEngine.Random;
 
 public class SteeringInputMockup : MonoBehaviour
 {
-    [SerializeField] AttitudeController attitudeController;
+    private INeutralRollController _rollController;
+    private IHeadingController _headingController;
+    private Rigidbody _rb;
+
+    private void Awake()
+    {
+        _rollController = GetComponent<INeutralRollController>();
+        _headingController = GetComponent<IHeadingController>();
+        _rb = GetComponent<Rigidbody>();
+    }
 
     private void OnGUI()
     {
         if (GUILayout.Button("Neutral roll"))
         {
-            attitudeController?.RollToNeutral();
+            _rollController?.RollToNeutral();
         }
 
         if (GUILayout.Button("Random Attitude"))
         {
+            _rollController?.StopRoll();
             transform.rotation = Random.rotation;
         }
+
+        if (GUILayout.Button("Heading test"))
+        {
+            _headingController?.ChangeHeadingTo(Vector3.right);
+        }
+        GUILayout.Label(_rb.angularVelocity.magnitude.ToString(CultureInfo.InvariantCulture));
+        
     }
 
-    private void Start()
-    {
-        attitudeController?.RollToNeutral();
-    }
+    
 }
